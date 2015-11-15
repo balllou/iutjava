ff17d921a71e512b109b067caf1e55ae0f46ac38
balllou <alex.dolsa@gmail.com>
2015-11-15 15:41:51 +0100

    ajout des repertoires train et workspace

---
+++ workspace/iuttp/src/edu/iut/app/ApplicationErrorLog.java
@@ -0,0 +1,25 @@
+package edu.iut.app;
+
+public abstract  class ApplicationErrorLog extends AbstractApplicationLog {
+
+	/** Ajouter un tableau ou profiter de l'héritage ? */
+	public ApplicationErrorLog() {
+		super();
+	}
+
+	@Override
+	public void setMessage(String message) {
+		this.message = message;
+		super.fireMessage("[ERROR]", this.message);
+	}
+
+
+
+	@Override
+	public String getMessage() {
+		// TODO Auto-generated method stub
+		return this.message;
+	}
+
+
+}
