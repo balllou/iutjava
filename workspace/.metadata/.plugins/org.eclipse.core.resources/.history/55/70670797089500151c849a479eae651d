4a3fa1d2ff90c7023ad30f701b15ba9b497d5db9
balllou <alex.dolsa@gmail.com>
2015-11-27 14:16:11 +0100

    modif classe critereSalle

---
+++ workspace/.metadata/.plugins/org.eclipse.core.resources/.history/55/70670797089500151c849a479eae651d
@@ -0,0 +1,13 @@
+package edu.iut.app;
+
+import java.util.LinkedList;
+
+public class CritereSalle implements Critere{
+
+	@Override
+	public LinkedList<ExamEvent> meetCriteria(LinkedList<ExamEvent> events) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- workspace/iuttp/src/edu/iut/app/CritereSalle.java
+++ workspace/iuttp/src/edu/iut/app/CritereSalle.java
@@ -4,6 +4,17 @@ import java.util.LinkedList;
 
 public class CritereSalle implements Critere{
 
+	LinkedList<ExamEvent> salles = new LinkedList<ExamEvent>();
+	public LinkedList<ExamEvent> meetCriteria(LinkedList<ExamEvent> events,String numSalle) {
+		for (ExamEvent E : events) {
+			if(E.toString().equalsIgnoreCase(numSalle)){
+				salles.add(E);
+			}
+		}
+		return salles;
+	}
+	
+
 	@Override
 	public LinkedList<ExamEvent> meetCriteria(LinkedList<ExamEvent> events) {
 		// TODO Auto-generated method stub
