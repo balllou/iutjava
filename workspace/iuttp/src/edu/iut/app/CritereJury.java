ad82780a60d4300a1f4aed5981503eab680c26f7
balllou <alex.dolsa@gmail.com>
2015-11-27 14:05:42 +0100

    modif classes criterestudent/jury/none

--- workspace/iuttp/src/edu/iut/app/CritereJury.java
+++ workspace/iuttp/src/edu/iut/app/CritereJury.java
@@ -1,5 +1,19 @@
 package edu.iut.app;
 
-public class CritereJury {
+import java.util.LinkedList;
+
+public class CritereJury implements Critere{
+
+	LinkedList<ExamEvent> jury = new LinkedList<ExamEvent>();
+	
+		@Override
+		public LinkedList<ExamEvent> meetCriteria(LinkedList<ExamEvent> events) {
+			for (ExamEvent E : events) {
+				if(E.toString().equalsIgnoreCase("JURY")){
+					jury.add(E);
+				}
+			}
+			return jury;
+	}
 
 }
--- workspace/iuttp/src/edu/iut/app/CritereNone.java
+++ workspace/iuttp/src/edu/iut/app/CritereNone.java
@@ -1,5 +1,19 @@
 package edu.iut.app;
 
-public class CritereNone {
+import java.util.LinkedList;
+
+public class CritereNone implements Critere{
+	
+	LinkedList<ExamEvent> none = new LinkedList<ExamEvent>();
+	
+	@Override
+	public LinkedList<ExamEvent> meetCriteria(LinkedList<ExamEvent> events) {
+		for (ExamEvent E : events) {
+			if(E.toString().equalsIgnoreCase("NONE")){
+				none.add(E);
+			}
+		}
+		return none;
+}
 
 }
